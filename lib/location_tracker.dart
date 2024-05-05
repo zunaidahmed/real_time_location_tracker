35a97e6b7baf356eb68ea8e6ce4783d42b978c9b
zunaid <zunaidahmmed@gmail.com>
2024-05-05 18:20:05 +0600

    Real Time Location Tracker

--- lib/location_tracker.dart
+++ lib/location_tracker.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -23,11 +24,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController mapController;
   Position? currentPosition;
+  late StreamSubscription<Position> positionStreamSubscription;
 
   @override
   void initState() {
     super.initState();
     _getUserLocation();
+    _startLocationUpdates();
+  }
+
+  @override
+  void dispose() {
+    positionStreamSubscription.cancel(); // Cancel the position stream subscription
+    super.dispose();
   }
 
   void _getUserLocation() async {
@@ -42,13 +51,27 @@ class _HomeScreenState extends State<HomeScreen> {
       });
     } else {
       LocationPermission permissionRequested =
-          await Geolocator.requestPermission();
+      await Geolocator.requestPermission();
       if (permissionRequested == LocationPermission.always ||
           permissionRequested == LocationPermission.whileInUse) {
         _getUserLocation();
       }
     }
   }
+
+  void _startLocationUpdates() {
+    const interval = Duration(seconds: 10);
+
+    Timer.periodic(interval, (Timer timer) async {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+      );
+      setState(() {
+        currentPosition = position;
+      });
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -63,56 +86,56 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: currentPosition != null
           ? GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                  currentPosition!.latitude,
-                  currentPosition!.longitude,
-                ),
-                zoom: 16,
-              ),
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              onMapCreated: (GoogleMapController controller) {
-                mapController = controller;
-              },
-              markers: {
-                Marker(
-                  markerId: MarkerId('my-current-location'),
-                  position: LatLng(
-                    currentPosition!.latitude,
-                    currentPosition!.longitude,
-                  ),
-                  infoWindow: InfoWindow(
-                    title: 'My Current Location',
-                    snippet:
-                        '${currentPosition!.latitude}${currentPosition!.longitude}',
-                  ),
-                ),
-                const Marker(
-                  markerId: MarkerId('my-previous-location'),
-                  position: LatLng(23.79318, 90.29625),
-                  infoWindow: InfoWindow(
-                    title: 'My Previous Location',
-                    snippet: '23.79318,90.29625',
-                  ),
-                ),
-              },
-              polylines: {
-                Polyline(
-                  polylineId: PolylineId('poly_one'),
-                  color: Colors.blueAccent,
-                  width: 5,
-                  points: [
-                    LatLng(
-                        currentPosition!.latitude, currentPosition!.longitude),
-                    LatLng(23.79318, 90.29625),
-                  ],
-                ),
-              },
-            )
-          : const Center(
-              child: CircularProgressIndicator(),
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            currentPosition!.latitude,
+            currentPosition!.longitude,
+          ),
+          zoom: 16,
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+        markers: {
+          Marker(
+            markerId: MarkerId('my-current-location'),
+            position: LatLng(
+              currentPosition!.latitude,
+              currentPosition!.longitude,
             ),
+            infoWindow: InfoWindow(
+              title: 'My Current Location',
+              snippet:
+              '${currentPosition!.latitude}${currentPosition!.longitude}',
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('my-previous-location'),
+            position: LatLng(23.79318, 90.29625),
+            infoWindow: InfoWindow(
+              title: 'My Previous Location',
+              snippet: '23.79318,90.29625',
+            ),
+          ),
+        },
+        polylines: {
+          Polyline(
+            polylineId: PolylineId('poly_one'),
+            color: Colors.blueAccent,
+            width: 5,
+            points: [
+              LatLng(
+                  currentPosition!.latitude, currentPosition!.longitude),
+              LatLng(23.79318, 90.29625),
+            ],
+          ),
+        },
+      )
+          :Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
